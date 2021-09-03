476677db199abb1e5417b184eca46e9d6d0a05a7
sakamototaisei <taisei0403089@gmail.com>
2021-09-03 15:39:06 +0000

    Rails　boolers2 [finish19]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configuer_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path(resource)
+    user_path(resource)
+  end
+
+  protected
+
+  def configuer_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
