b36ee7c8639094bd701c6ebfcf0b446ce6d66e3c
sakamototaisei <taisei0403089@gmail.com>
2021-09-06 14:49:02 +0000

    RailsBookers[finish44]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,8 +10,12 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book)
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
 
@@ -23,10 +27,17 @@ class BooksController < ApplicationController
 
 
   def edit
+    @book = Book.find(params[:id])
   end
 
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: "You have updated book successfully."
+    else
+      render :edit
+    end
   end
 
 
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '2035467417d6159e647d4b944e2870d4680387e4da2d788acce0eda33a2d2cfca0a4672f23b53bced59c943c54d3e6abf668418073d4d7fbbcd18013f73c7c0c'
