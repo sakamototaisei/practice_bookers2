f53057d03d4091db2d4591ecd4d3cebbddcc4d22
sakamototaisei <taisei0403089@gmail.com>
2021-09-09 14:19:40 +0000

    Rails Bookers2 いいね機能完成

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,15 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true
   validates :body, length: {maximum: 200}
+
+  # 引数で渡されたユーザーidがfavoritesテーブル内に存在(exists?)
+  # するかを調べる。あればtrue,なければfalseを返す
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 end
