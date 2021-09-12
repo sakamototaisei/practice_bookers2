6202868a8f8dc65dcc9d73593e9e6fe69ccd5a61
sakamototaisei <taisei0403089@gmail.com>
2021-09-12 15:22:51 +0000

    Rails Bookers2 gem無し検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'searches/search'
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
+  get '/search' => 'searches#search'
   resources :users, only: [:index, :show, :edit, :update] do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
