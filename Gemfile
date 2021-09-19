62b2faa667f4ed6b7d653151345ea3f25c691ada
sakamototaisei <taisei0403089@gmail.com>
2021-09-19 14:10:06 +0000

    Rails Bookers2 カミナリ機能追加

--- Gemfile
+++ Gemfile
@@ -67,5 +67,6 @@ gem 'devise'
 gem 'bootstrap', '~> 4.5'
 gem 'jquery-rails'
 gem 'font-awesome-sass', '~> 5.13'
+gem 'kaminari', '~> 1.2.1'
 
 gem 'dotenv-rails'
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
 
   def index
     @book = Book.new
-    @books = Book.all
+    @books = Book.page(params[:page]).reverse_order
     @user = current_user
   end
 
